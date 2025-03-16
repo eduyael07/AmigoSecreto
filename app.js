cc107dbbfe951a1d6769b1b2b88b46c49ffc8357
Eduardo Jimenez <edujimenez621@gmail.com>
2025-03-16 16:44:27 -0600

    Func para agregar amigos y act lista

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = []  //Inicializar lista para nombres
 
+
+document.addEventListener("DOMContentLoaded", function() {
+    let amigos = []  //Inicializar lista para nombres
+
+    document.getElementById("Agregar").addEventListener("click", function(){
+        let inputNombre = document.getElementById("Nombre");
+        let nombre = inputNombre.value.trim();
+
+        if (nombre === ""){
+            alert("Por favor, ingresa un nombre.");
+        } else {
+            amigos.push(nombre);
+            inputNombre.value = "";
+           actualizarLista();
+        }
+    });
+
+    function actualizarLista() {
+        let lista = document.getElementById("lista-amigos");
+        lista.innerHTML = "";
+        
+        for (let i=0; i < amigos.length; i++) {
+            let elemento = document.createElement("li");
+            elemento.textContent = amigos[i];
+            lista.appendChild(elemento);
+        }
+    }
+}
+);
 
